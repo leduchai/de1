6b13bf25a028b127fdbeec48b18efe967f58f4fa
leduchai <leduchaif@gmail.com>
2020-10-15 15:03:31 +0700

    third commit

--- WebApplication1/Models/ErrorViewModel.cs
+++ WebApplication1/Models/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace WebApplication1.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Name { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
