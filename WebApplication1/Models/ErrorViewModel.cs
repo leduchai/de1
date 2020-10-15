c3e4e7535d279093d4c3db33b6427b4a79f9950e
123 <123@gmail.com>
2020-10-15 15:29:48 +0700

    eight

--- WebApplication1/Models/ErrorViewModel.cs
+++ WebApplication1/Models/ErrorViewModel.cs
@@ -5,7 +5,7 @@ namespace WebApplication1.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-        public string Name { get; set; }
+        public string FullName { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
